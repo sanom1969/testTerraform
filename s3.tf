3b41ebce067e5d7187ccfd13b471c09239ce3db7
Masa <m.ework1969@gmail.com>
2018-07-09 18:00:40 +0900

    Updated

--- s3.tf
+++ s3.tf
@@ -1,9 +1,8 @@
-resource "aws_s3_bucket" "terraform_tutorial" {
-    bucket = "xyz-abc"
-    acl = "private"
-    versioning = {
-        enabled = true
-    }
-}
+resource "aws_s3_bucket" "terraform_tfstate_strl" {
+  bucket = "tfstate-management-sanom-strl"
+  acl    = "private"
 
-    
+  versioning = {
+    enabled = true
+  }
+}
